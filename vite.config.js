b8b01bf4ac668ece6b9e96885eb4b45a286ae2ca
Matěj Štágl <stagl.matej@centrum.cz>
2024-09-16 01:46:38 +0200

    setup vite, rollup

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite'
+
+const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'))
+const header = `
+/**
+ * Plotter.js
+ * Version: ${packageJson.version}
+ * Author: ${packageJson.author}
+ * License: ${packageJson.license}
+ */
+`
+
+// Vlastní plugin pro přidání hlavičky
+function addHeaderPlugin() {
+    return {
+      name: 'add-header',
+      generateBundle(options, bundle) {
+        for (const fileName in bundle) {
+          const chunk = bundle[fileName]
+          if (chunk.type === 'chunk' && chunk.isEntry) {
+            chunk.code = header + chunk.code
+          }
+        }
+      }
+    }
+}
+
+export default defineConfig({
+    build: {
+      manifest: true,
+      rollupOptions: {
+        input: '/plotter.js',
+        output: {
+            entryFileNames: 'plotter.js'
+        },
+        plugins: [
+            addHeaderPlugin()
+        ]
+      }
+    }
+  })
